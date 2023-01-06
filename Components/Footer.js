e86ed1006fa430374a0635e7c848c738a71b5b49
David Waldherr <david.a.waldherr@vanderbilt.edu>
2023-01-05 19:29:17 -0600

    button

--- Components/Footer.js
+++ Components/Footer.js
@@ -11,9 +11,12 @@ export const Footer = (color) => {
         <Stack spacing="4" direction={{ base: 'column', md: 'row' }} align="center" justifyContent="center">
           <Stack spacing="4" direction={{ base: 'row', md: 'row' }} maxW={{ lg: '360px' }}>
             <Input variant="flushed" borderBottomWidth="3px" borderBottomColor="black" placeholder="Email Address" type="email" required />
+            
+            <form action="https://iamfritzhager.us21.list-manage.com/subscribe/post?u=fa20112546e7ab40f40f0bcb6&amp;id=81e7398c39&amp;f_id=0033cce1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <Button variant="ghost" borderColor="black" borderWidth="3px" type="submit" flexShrink={0}>
               Join
             </Button>
+            </form>
           </Stack>
           <Box position="relative" width={width}></Box>
           <Box
